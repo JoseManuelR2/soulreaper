7e11fa63b5e5bea330f4ecc67d8301e007c32cd9
josemri <josepm.ramos.i@gmail.com>
2026-03-23 12:04:36 +0100

    el problema ahora es que los zombie se fuman el suelo del main y lagazo

--- Assets/scripts/LevelSelector.cs
+++ Assets/scripts/LevelSelector.cs
@@ -6,11 +6,14 @@ public class LevelSelector : MonoBehaviour
 {
     public Object level1Scene;
     public Object level2Scene;
+    private bool isLoading = false;
 
     private string currentSceneLoaded = "";
 
     private void OnTriggerEnter(Collider other)
     {
+        if (isLoading) return; // 👈 BLOQUEO
+
         Transform parent = other.transform.parent;
 
         if (parent != null && parent.CompareTag("Interactable"))
@@ -34,6 +37,8 @@ public class LevelSelector : MonoBehaviour
         if (currentSceneLoaded == sceneName)
             yield break;
 
+        isLoading = true; // 👈 EMPIEZA BLOQUEO
+
         // Descargar escena anterior
         if (!string.IsNullOrEmpty(currentSceneLoaded))
         {
@@ -41,25 +46,49 @@ public class LevelSelector : MonoBehaviour
             yield return unloadOp;
         }
 
-        // Cargar nueva escena SIN activarla aún
         AsyncOperation loadOp = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
         loadOp.allowSceneActivation = false;
 
-        // Carga progresiva
         while (loadOp.progress < 0.9f)
         {
             Debug.Log("Cargando: " + (loadOp.progress * 100f) + "%");
             yield return null;
         }
 
-        // Aquí puedes meter un fade o esperar
         yield return new WaitForSeconds(0.2f);
 
-        // Activar escena
         loadOp.allowSceneActivation = true;
+
+        yield return null;
+
+        GameObject door = GameObject.Find("Door_02_reinforced");
+
+        if (door != null)
+        {
+            StartCoroutine(RotateDoor(door.transform, 3.3f, 100f));
+        }
 
         currentSceneLoaded = sceneName;
 
         Debug.Log("Escena cargada: " + sceneName);
+
+        isLoading = false; // 👈 DESBLOQUEO
+    }
+
+    IEnumerator RotateDoor(Transform door, float duration, float angle)
+    {
+        Quaternion startRotation = door.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0, angle, 0);
+
+        float time = 0f;
+
+        while (time < duration)
+        {
+            door.rotation = Quaternion.Slerp(startRotation, endRotation, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        door.rotation = endRotation;
     }
 }
